f47076bc28df1e6951207bf30aca920dbd34f1c7
승현 <128073991+seunghyun333@users.noreply.github.com>
2023-06-11 00:43:30 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.34 ms, Memory: 69.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -1,20 +1,10 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 0;
-        int max1 = Integer.MIN_VALUE;
-        int max2 = Integer.MIN_VALUE;
 
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] > max1) {
-                max2 = max1;
-                max1 = numbers[i];
-            } else if (numbers[i] > max2) {
-                max2 = numbers[i];
-            }
-        }
-
-        answer = max1 * max2;
-
+        Arrays.sort(numbers);
+        int answer = numbers[numbers.length -1] * numbers[numbers.length -2];
         return answer;
     }
 }

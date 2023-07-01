bc181d217b47ff46faf12fa4435fdcfd80c4adbb
승현 <128073991+seunghyun333@users.noreply.github.com>
2023-07-01 23:25:46 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.60 ms, Memory: 83.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+class Solution {
+    public String solution(String my_string) {
+        String answer = my_string.toLowerCase();
+        
+        char[] charArray = answer.toCharArray();
+        
+        Arrays.sort(charArray);
+        
+        return new String(charArray);
+    }
+}

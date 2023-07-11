7b82c3223921d2f177b75f71e0dd21f31eb69c81
승현 <128073991+seunghyun333@users.noreply.github.com>
2023-07-11 21:35:52 +0900

    [unrated] Title: 커피 심부름, Time: 0.03 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.java
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.java
@@ -6,13 +6,11 @@ class Solution {
         int b = 5000;
         
         for (int i = 0; i < order.length; i++) {
-            if (order[i].contains("americano")) {
+            if (order[i].contains("americano") || order[i].contains("anything")) {
                 answer += a;
             } else if (order[i].contains("latte")) {
                 answer += b;
-            } else if (order[i].contains("anything")) {
-                answer += a;
-            } 
+            }
         }
         
         return answer;

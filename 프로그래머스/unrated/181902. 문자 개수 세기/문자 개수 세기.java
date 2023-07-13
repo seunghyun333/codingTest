13a3f40374ee05464874aef97c2253bdb3dc2a69
승현 <128073991+seunghyun333@users.noreply.github.com>
2023-07-13 17:19:59 +0900

    [unrated] Title: 문자 개수 세기, Time: 0.02 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.java
@@ -0,0 +1,115 @@
+class Solution {
+    public int[] solution(String my_string) {
+        int[] answer = new int[52];
+        
+        for (int i = 0; i < my_string.length(); i++) {
+            if (my_string.charAt(i) == 'A') {
+                answer[0]++;
+            } else if (my_string.charAt(i) == 'B') {
+                answer[1]++;
+            } else if (my_string.charAt(i) == 'C') {
+                answer[2]++;
+            } else if (my_string.charAt(i) == 'D') {
+                answer[3]++;
+            } else if (my_string.charAt(i) == 'E') {
+                answer[4]++;
+            } else if (my_string.charAt(i) == 'F') {
+                answer[5]++;
+            } else if (my_string.charAt(i) == 'G') {
+                answer[6]++;
+            } else if (my_string.charAt(i) == 'H') {
+                answer[7]++;
+            } else if (my_string.charAt(i) == 'I') {
+                answer[8]++;
+            } else if (my_string.charAt(i) == 'J') {
+                answer[9]++;
+            } else if (my_string.charAt(i) == 'K') {
+                answer[10]++;
+            } else if (my_string.charAt(i) == 'L') {
+                answer[11]++;
+            } else if (my_string.charAt(i) == 'M') {
+                answer[12]++;
+            } else if (my_string.charAt(i) == 'N') {
+                answer[13]++;
+            } else if (my_string.charAt(i) == 'O') {
+                answer[14]++;
+            } else if (my_string.charAt(i) == 'P') {
+                answer[15]++;
+            } else if (my_string.charAt(i) == 'Q') {
+                answer[16]++;
+            } else if (my_string.charAt(i) == 'R') {
+                answer[17]++;
+            } else if (my_string.charAt(i) == 'S') {
+                answer[18]++;
+            } else if (my_string.charAt(i) == 'T') {
+                answer[19]++;
+            } else if (my_string.charAt(i) == 'U') {
+                answer[20]++;
+            } else if (my_string.charAt(i) == 'V') {
+                answer[21]++;
+            } else if (my_string.charAt(i) == 'W') {
+                answer[22]++;
+            } else if (my_string.charAt(i) == 'X') {
+                answer[23]++;
+            } else if (my_string.charAt(i) == 'Y') {
+                answer[24]++;
+            } else if (my_string.charAt(i) == 'Z') {
+                answer[25]++;
+            } else if (my_string.charAt(i) == 'a') {
+                answer[26]++;
+            } else if (my_string.charAt(i) == 'b') {
+                answer[27]++;
+            } else if (my_string.charAt(i) == 'c') {
+                answer[28]++;
+            } else if (my_string.charAt(i) == 'd') {
+                answer[29]++;
+            } else if (my_string.charAt(i) == 'e') {
+                answer[30]++;
+            } else if (my_string.charAt(i) == 'f') {
+                answer[31]++;
+            } else if (my_string.charAt(i) == 'g') {
+                answer[32]++;
+            } else if (my_string.charAt(i) == 'h') {
+                answer[33]++;
+            } else if (my_string.charAt(i) == 'i') {
+                answer[34]++;
+            } else if (my_string.charAt(i) == 'j') {
+                answer[35]++;
+            } else if (my_string.charAt(i) == 'k') {
+                answer[36]++;
+            } else if (my_string.charAt(i) == 'l') {
+                answer[37]++;
+            } else if (my_string.charAt(i) == 'm') {
+                answer[38]++;
+            } else if (my_string.charAt(i) == 'n') {
+                answer[39]++;
+            } else if (my_string.charAt(i) == 'o') {
+                answer[40]++;
+            } else if (my_string.charAt(i) == 'p') {
+                answer[41]++;
+            } else if (my_string.charAt(i) == 'q') {
+                answer[42]++;
+            } else if (my_string.charAt(i) == 'r') {
+                answer[43]++;
+            } else if (my_string.charAt(i) == 's') {
+                answer[44]++;
+            } else if (my_string.charAt(i) == 't') {
+                answer[45]++;
+            } else if (my_string.charAt(i) == 'u') {
+                answer[46]++;
+            } else if (my_string.charAt(i) == 'v') {
+                answer[47]++;
+            } else if (my_string.charAt(i) == 'w') {
+                answer[48]++;
+            } else if (my_string.charAt(i) == 'x') {
+                answer[49]++;
+            } else if (my_string.charAt(i) == 'y') {
+                answer[50]++;
+            } else if (my_string.charAt(i) == 'z') {
+                answer[51]++;
+            } 
+        }
+        
+        return answer;
+    }
+}

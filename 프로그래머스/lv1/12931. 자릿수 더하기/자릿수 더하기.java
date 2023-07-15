6df970af22907724ab95b36d4ce2283c898751c0
승현 <128073991+seunghyun333@users.noreply.github.com>
2023-07-16 01:27:46 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String str = Integer.toString(n);
+        
+        for(int i = 0; i < str.length(); i++){
+            answer = answer + Integer.valueOf(String.valueOf(str.charAt(i)));
+        }
+        
+
+        return answer;
+    }
+}

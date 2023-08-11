c46df57d029d17a713c6f6b5e62a0727d75be4bf
승현 <128073991+seunghyun333@users.noreply.github.com>
2023-08-11 12:31:48 +0900

    Update 캐릭터의 좌표.java

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -3,7 +3,10 @@ class Solution {
         int[] answer = new int[2]; 
         int posX = 0; 
         int posY = 0;
-            
+
+
+         //Math.max(posX - 1, -board[0] / 2)는 두 값 중에서 더 큰 값을 선택하는 함수
+        
         for (int i = 0; i < keyinput.length; i++) {
             switch (keyinput[i]) {
                 case "left":

e00a02b8b751288eab66aac208c912a9a08995d8
승현 <128073991+seunghyun333@users.noreply.github.com>
2024-02-16 21:56:17 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17792 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] arts) {
+    
+    Scanner scanner = new Scanner(System.in);
+    
+    int a = scanner.nextInt();
+    int result = a - 543;
+    
+    System.out.println(result);
+    
+    scanner.close();
+    }
+}
+
